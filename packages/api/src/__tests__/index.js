e8868beaee5325a3143138733e14b6c1bf79413e
Cory Reed <swashcap@gmail.com>
2018-06-24 15:06:02 -0700

    test(api): add basic server test

---
+++ packages/api/src/__tests__/index.js
@@ -0,0 +1,18 @@
+const supertest = require('supertest')
+const app = require('../')
+
+/**
+ * Send a basic introspection query to test everything's wired up correctly.
+ * {@link https://graphql.org/learn/introspection/}
+ */
+test('graphql route', () => supertest(app)
+  .post('/graphql')
+  .send({
+    query: '{ __schema { types { name } } }'
+  })
+  .expect(200)
+  .then(({ body }) => {
+    expect(body.data && body.__schema);
+  })
+)
+
