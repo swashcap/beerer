937f822bf2ad2146e428c778d4155404a2f203cc
Cory Reed <swashcap@gmail.com>
2018-06-24 13:51:19 -0700

    fix(pkg): force loading of .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = {
   module: {
     rules: [{
       exclude: /node_modules/,
       loader: 'babel-loader',
-      options: {
-        cacheDirectory: true
-      },
+      /**
+       * babel-loader doesn't load the .babelrc
+       * @todo Remove once the issue is addressed
+       * {@link https://github.com/babel/babel-loader/issues/624}
+       */
+      options: Object.assign(
+        {
+          babelrc: false,
+          cacheDirectory: true
+        },
+        JSON.parse(fs.readFileSync(path.join(__dirname, '.babelrc'), 'utf-8'))
+      ),
       test: /\.js$/
     }, {
       exclude: /node_modules/,
