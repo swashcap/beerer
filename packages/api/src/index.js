f555c6a3098af6170af0832df0c26875257c00ff
Cory Reed <swashcap@gmail.com>
2018-06-24 00:44:48 -0700

    feat(pkg): wire up starter GraphQL server

---
+++ packages/api/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const morgan = require('morgan')
+const schema = require('beerer-schema')
+const { buildSchema } = require('graphql')
+
+const resolvers = require('./resolvers/index.js')
+
+const IS_DEV = !(process.env.NODE_ENV === 'production')
+const PORT = process.env.PORT || 4000;
+
+const app = express()
+
+app.use(morgan(IS_DEV ? 'dev' : 'short'))
+app.use('/graphql', graphqlHTTP({
+  graphiql: IS_DEV,
+  pretty: IS_DEV,
+  rootValue: resolvers,
+  schema: buildSchema(schema)
+}))
+
+if (require.main === module) {
+  app.listen(PORT, (error) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log(`Server listening at http://localhost:${PORT}`)
+    }
+  })
+}
+
+module.exports = app
