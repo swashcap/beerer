282967bba6a938261f222b61ece4c08b09041a14
Cory Reed <swashcap@gmail.com>
2018-07-01 16:13:49 -0700

    feat(pkg): add fixture data to API

--- packages/api/src/resolvers/index.js
+++ packages/api/src/resolvers/index.js
@@ -1,3 +1,34 @@
+const beers = require('../../fixtures/beers.json')
+const breweries = require('../../fixtures/breweries.json')
+
+/**
+ * Get a formatted `Brewery` from its ID.
+ *
+ * @param {String} id
+ * @returns {Object} Brewery
+ */
+const getBrewery = (id) => {
+  if (!(id in breweries)) {
+    throw new Error(`Could not find brewery ID ${id}`)
+  }
+
+  const brewery = breweries[id]
+
+  return {
+    id,
+    location: `${brewery.location.city}, ${brewery.location.state}`,
+    name: brewery.name,
+    url: brewery.url
+  }
+}
+
+/**
+ * @returns {Object[]} Beers
+ */
+const getBeers = () => beers.map((beer) => Object.assign({}, beer, {
+  brewery: beer.brewery.split(',').map(getBrewery)
+}))
+
 module.exports = {
-  hello: () => 'Hello world!'
+  beers: getBeers
 }
